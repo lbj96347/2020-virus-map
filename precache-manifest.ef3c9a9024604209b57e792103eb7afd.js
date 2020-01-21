ab576dd404878f79eb18cf654f0abace01312d00
CashLee <cashlee96347@gmail.com>
2020-01-21 13:53:13 +0800

    [fix]fix env variable

---
+++ precache-manifest.ef3c9a9024604209b57e792103eb7afd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8f6c0d850115e2221af88ad03a5e92",
+    "url": "https://lbj96347.github.io/2020-virus-map/index.html"
+  },
+  {
+    "revision": "05b23e87d56623af6efa",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/css/main.22ae4f51.chunk.css"
+  },
+  {
+    "revision": "54f4f1369b544426f7e4",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/2.9cd92c74.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/2.9cd92c74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05b23e87d56623af6efa",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/main.70df61e8.chunk.js"
+  },
+  {
+    "revision": "5d108f6380ee42bb8c41",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/runtime-main.7590d12d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://lbj96347.github.io/2020-virus-map/precache-manifest.4b876016873997234f8bf2cac7913e0c.js"
+  "https://lbj96347.github.io/2020-virus-map/precache-manifest.ef3c9a9024604209b57e792103eb7afd.js"
 );
 
 self.addEventListener('message', (event) => {
