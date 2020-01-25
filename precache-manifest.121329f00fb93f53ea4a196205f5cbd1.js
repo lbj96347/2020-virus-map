38dd95f3852526a0d305072f6b035d36c2a1dc8b
CashLee <cashlee96347@gmail.com>
2020-01-25 15:13:54 +0800

    [fix]fix env variable

---
+++ precache-manifest.121329f00fb93f53ea4a196205f5cbd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e0c85bf91e922013c00ca6daaa906a",
+    "url": "https://lbj96347.github.io/2020-virus-map/index.html"
+  },
+  {
+    "revision": "1b6f8ac499d385a6deff",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/css/main.4fb5c3e6.chunk.css"
+  },
+  {
+    "revision": "54f4f1369b544426f7e4",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/2.9cd92c74.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/2.9cd92c74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b6f8ac499d385a6deff",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/main.58fa408b.chunk.js"
+  },
+  {
+    "revision": "5d108f6380ee42bb8c41",
+    "url": "https://lbj96347.github.io/2020-virus-map/static/js/runtime-main.7590d12d.js"
+  }
+]);
